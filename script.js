5a8c5a3c55898befc1f59b07726158f98e6afa99
Himmu9131 <114733178+Himmu9131@users.noreply.github.com>
2025-01-04 21:38:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function mincost(arr)
 //write your code here
 // return the min cost
 	let minCost=0;
-	arr.sort((a,b)=>(a-b)
+	arr.sort((a,b)=>(a-b));
 	while(arr.length>1){
 		let first=arr.shift();
 		let second=arr.shift();
