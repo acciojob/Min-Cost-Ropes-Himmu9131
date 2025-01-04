6c3b15b09e3cdf2f11c9f5260db88b6b640e528c
Himmu9131 <114733178+Himmu9131@users.noreply.github.com>
2025-01-04 21:20:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	let minCost=0;
+	arr.sort((a,b)=>(a-b)
+	while(arr.length>1){
+		let first=arr.shift();
+		let second=arr.shift();
+		minCost=first+second;
+		arr.push(minCost);
+		arr.sort((a,b)=>a-b);
+		
+	}
+	return minCost;
 }
 
 module.exports=mincost;
